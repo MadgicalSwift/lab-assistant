a44e4db6a6942ef8605b8b0380118732e683355b
HeliosHorizon <2100331540038@rkgit.edu.in>
2024-11-15 17:16:51 +0530

    updated-data

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingService } from './common/middleware/logger.middleware';
+import axios from 'axios';
+
+axios.defaults.timeout = 10000;   // 10 seconds
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('/lab-assistant/v1');
   await app.listen(3000);
 }
 bootstrap();
